626bd14be24c349672124f4771ac6ba8b600d27e
Petr Klimenko <petrklim@yandex.ru>
2020-01-05 22:57:15 +0300

    Small fixes

--- src/Address.ts
+++ src/Address.ts
@@ -1,13 +1,5 @@
 import { AddressType, GeocodingAddressComponentType } from '@google/maps'
 
-export type Address = {
-  house: string
-  street: string
-  city: string
-  state: string
-  zip: string
-}
-
 type ComponentForm = 'short_name' | 'long_name'
 
 export type AddressFieldsMapping<T> = {
